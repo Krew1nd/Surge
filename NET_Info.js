57d00610362215ff2042ea11eebb2124b1f71789
Krew1nd <87192031+Krew1nd@users.noreply.github.com>
2022-04-10 15:09:32 +0800

    Update NET_Info.js

--- NET_Info.js
+++ NET_Info.js
@@ -17,40 +17,40 @@ $httpClient.get('http://ip-api.com/json/?lang=zh-CN', function (error, response,
     const jsonData = JSON.parse(data);
   switch (`${jsonData.org}`){
     case "":
-      if (`${jsonData.as}` != "") {
+      if (`${jsonData.isp}` != "") {
       $done({
-      title:rootName,
+      title:节点信息,
       content:
                 `IP：${jsonData.query}\n` + `AS：${jsonData.as}\n` + `地区：${jsonData.country}`,
-      icon: "info.circle",
-       "icon-color":"#369CF3",
+      icon: "info.circle.fill",
+       "icon-color":"#FF007AFF",
     });
       } else {
       $done({
-      title:rootName,
+      title:节点信息,
       content:
 		`IP：${jsonData.query}\n` + `地区：${jsonData.country}`,
-      icon: "info.circle",
-       "icon-color":"#369CF3",
+      icon: "info.circle.fill",
+       "icon-color":"#FF007AFF",
     });
       }
       break;
     default:
-      if (`${jsonData.as}` != "" && `${jsonData.isp}` != `${jsonData.org}`) {
+      if (`${jsonData.isp}` != "" && `${jsonData.isp}` != `${jsonData.org}`) {
       $done({
-      title:rootName,
+      title:节点信息,
       content:
 		`IP：${jsonData.query}\n` + `AS：${jsonData.as}\n` + `数据中心：${jsonData.org}`,
-      icon: "info.circle",
-       "icon-color":"#369CF3",
+      icon: "info.circle.fill",
+       "icon-color":"#FF007AFF",
     });
       } else {
       $done({
-      title:rootName,
+      title:节点信息,
       content:
 		`IP：${jsonData.query}\n` + `AS：${jsonData.as}\n` + `地区：${jsonData.country}`,
-      icon: "info.circle",
-       "icon-color":"#369CF3",
+      icon: "info.circle.fill",
+       "icon-color":"#FF007AFF",
      });
     }
    }
