bad52603bc786cad6da3c6928944047b77f84436
Krew1nd <87192031+Krew1nd@users.noreply.github.com>
2022-08-08 12:23:29 +0800

    Update Sub_Info_Panel.js

--- Sub_Info_Panel.js
+++ Sub_Info_Panel.js
@@ -42,15 +42,15 @@ let args = getArgs();
   let used = info.download + info.upload;
   let total = info.total;
   let expire = args.expire || info.expire;
-  let content = [`• ${bytesToSize(used)} has been used`];
+  let content = [`· ${bytesToSize(used)} has been used`];
   
   if (resetDayLeft) {
-    content.push(`• ${resetDayLeft} left ${str} traffic reset`);
+    content.push(`· ${resetDayLeft} left ${str} traffic reset`);
   }
   
   if (expire && expire !== "false") {
     if (/^[\d.]+$/.test(expire)) expire *= 1000;
-    content.push(`• Next due date is ${formatTime(expire)}`);
+    content.push(`· Next due date is ${formatTime(expire)}`);
   }
 
   let now = new Date();
