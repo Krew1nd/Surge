b23561932de41277188e077d7b2250844b88bf85
Krew1nd <87192031+Krew1nd@users.noreply.github.com>
2022-08-31 19:04:23 +0800

    Add files via upload

---
+++ Auto_Join_TestFlight.js
@@ -0,0 +1,57 @@
+!(async () => {
+ids = $persistentStore.read('APP_ID')
+if (ids == '') {
+	$notification.post('所有TF已加入完毕','模块已自动关闭','')
+	$done($httpAPI('POST', '/v1/modules', {'Auto Join TestFlight': 'false'}))
+} else {
+	ids = ids.split(',')
+	for await (const ID of ids) {
+		await autoPost(ID)
+	}
+}
+$done()
+})();
+
+function autoPost(ID) {
+  let Key = $persistentStore.read('key')
+  let testurl = 'https://testflight.apple.com/v3/accounts/' + Key + '/ru/'
+  let header = {
+    'X-Session-Id': `${$persistentStore.read('session_id')}`,
+    'X-Session-Digest': `${$persistentStore.read('session_digest')}`,
+    'X-Request-Id': `${$persistentStore.read('request_id')}`
+  }
+  return new Promise(function(resolve) {
+    $httpClient.get({url: testurl + ID,headers: header}, function(error, resp, data) {
+      if (error === null) {
+        let jsonData = JSON.parse(data)
+        if (jsonData.data == null) {
+          console.log(ID + ' ' + jsonData.messages[0].message)
+          resolve();
+        } else if (jsonData.data.status == 'FULL') {
+          console.log(ID + ' ' + jsonData.data.message)
+          resolve();
+        } else {
+          $httpClient.post({url: testurl + ID + '/accept',headers: header}, function(error, resp, body) {
+            let jsonBody = JSON.parse(body)
+            $notification.post(jsonBody.data.name, 'TestFlight加入成功', '')
+            console.log(jsonBody.data.name + ' TestFlight加入成功')
+						ids = $persistentStore.read('APP_ID')
+						ids = ids.split(',')
+						ids.splice(ids.indexOf(ID), 1)
+						ids = ids.toString()
+						$persistentStore.write(ids,'APP_ID')
+						resolve()
+          });
+        }
+      } else {
+        if (error =='The request timed out.') {
+          resolve();
+        } else {
+          $notification.post('自动加入TestFlight', error,'')
+          console.log(ID + ' ' + error)
+          resolve();
+        }
+      }
+    })
+  })
+}
